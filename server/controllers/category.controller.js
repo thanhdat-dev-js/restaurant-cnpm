00941fc9eb2aef11f9779f25822619408852eef7
QuocDung <quocdungngole@gmail.com>
2021-10-11 15:49:00 +0700

    controller: category functions OK

--- server/controllers/category.controller.js
+++ server/controllers/category.controller.js
@@ -14,26 +14,31 @@ module.exports = {
     },
 
     putProduct(req, res) {
-        Category.updateOne({ products: req.params.productID }, (err, product) => {
+        Category.updateOne({ 'products._id': req.params.productID }, (err, product) => {
             // update
         });
     },
 
     deleteProduct(req, res) {
-        Category.find({ products: req.params.productID }, (err, product) => {
+        Category.find({ 'products._id': req.params.productID }, (err, product) => {
             // delete a product by its id
+        });
+    },
+
+    getCategory(req, res) {
+        Category.find({ _id: req.params.categoryID }, (err, thatCategory) => {
+           res.status(200).json(thatCategory); 
         });
     },
 
     async postCategory(req, res) {
         // Post new category
         try {
-            const ctype = req.params.ctype;
-            const existed = await Category.findOne({ type: ctype });
+            const existed = await Category.findOne({ type: req.body.type });
             if (existed) {
                 return res.status(418).json({
                     success: 0,
-                    message: "Category type existed."
+                    message: "Category existed."
                 })
             }
 
@@ -43,11 +48,12 @@ module.exports = {
                 products: req.body.products
             });
 
-            newCategory.save();
-            res.status(200).json({
-                success: 1,
-                message: newCategory
-            });
+            if (newCategory.save()) {
+                res.status(200).json({
+                    success: 1,
+                    message: newCategory
+                });
+            }
         }
         catch(err) {
             res.status(500).json({
@@ -59,29 +65,52 @@ module.exports = {
 
     async putCategory(req, res) {
         // Update existing category
-        const ctype = req.params.ctype;
-        const existed = await Category.findOne({ type: ctype });
+        try {
+            const categoryID = req.params.categoryID;
+            const existed = await Category.findOne({ _id: categoryID });
+    
+            if (!existed) {
+                return res.status(400).json({
+                    success: 0,
+                    message: "Category not found."
+                })
+            }
+    
+            existed.type = req.body.type || existed.type;
+            existed.imgURL = req.body.imgURL || existed.imgURL;
+            existed.products = req.body.products || existed.products;
 
-        if (!existed) {
-            return res.status(400).json({
-                success: 0,
-                message: "Category not found."
-            })
-        };
-
-        const updateContent = {
-            type: req.body.type,
-            imgURL: req.body.imgURL,
-            products: req.body.products
-        };
-
-        await Category.updateOne({ type: ctype }, updateContent);
+            if (await existed.save()) {
+                res.status(200).json(existed);
+            }
+            
+            // const updateContent = {
+            //     type: req.body.type,
+            //     imgURL: req.body.imgURL,
+            //     products: req.body.products
+            // };
+    
+            // await Category.updateOne({ _id: categoryID }, updateContent);
+        }
+        catch(err) {
+            res.status(500).json({ success: 0, message: err });
+        }
 
     },
 
     deleteCategory(req, res) {
         // Delete whole category
-        const ctype = req.params.ctype;
+        Category.deleteOne({ _id: req.params.categoryID }, (err) => {
+            if (err) {
+                res.status(500).json({
+                    sucess: 0,
+                    message: err
+                });
+            }
+            else {
+                res.status(200).json({ success: 1 });
+            }
+        });
 
     },
 
