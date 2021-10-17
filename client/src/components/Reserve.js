e372891453df44a7b23fbec21ca367a3cfdf2edf
Lý Thanh Bách <40271986+edwardly1002@users.noreply.github.com>
2021-10-17 19:25:31 +0700

    Add pages Reserve and ReserveList

---
+++ client/src/components/Reserve.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Reserve() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Reserve
---
+++ client/src/components/ReserveList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ReserveList() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ReserveList
