d93e02693c701f06f5232f2c190ce4573e70805f
QuocDung <quocdungngole@gmail.com>
2021-10-19 23:17:39 +0700

    admin putProduct chuyển sang dùng productID

--- server/routes/category.route.js
+++ server/routes/category.route.js
@@ -4,14 +4,15 @@ const validateAdmin = require('../middlewares/validateAdmin');
 const category = require('../controllers/category.controller');
 
 router.get('/category', category.getAllProducts);
+router.get('/category/:categoryID', category.getCategory)
 
-router.post('/category', validateAdmin, category.postCategory);
-router.post('/category/:ctype', validateAdmin, category.postProduct);
+router.post('/category', category.postCategory);
+router.post('/category/:categoryID', category.postProduct);
 
-router.put('/category/:ctype', validateAdmin, category.putCategory);
-router.put('/category/:ctype/:productID', validateAdmin, category.putProduct);
+router.put('/category/:categoryID', category.putCategory);
+router.put('/category/:categoryID/:productID', category.putProduct);
 
-router.delete('/category/:ctype', validateAdmin, category.deleteCategory);
-router.delete('/category/:ctype/:productID', validateAdmin, category.deleteProduct);
+router.delete('/category/:categoryID', category.deleteCategory);
+router.delete('/category/:categoryID/:productID', category.deleteProduct);
 
 module.exports = router;
