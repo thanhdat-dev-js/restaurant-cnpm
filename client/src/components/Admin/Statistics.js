3483dcd24738ef4473f292cfdf8b15d591c3dc1d
tvpluss <truongphuocst310301@gmail.com>
2021-10-20 16:00:51 +0700

    modified admin.controller

--- client/src/components/Admin/Statistics.js
+++ client/src/components/Admin/Statistics.js
@@ -2,5 +2,12 @@ import React from "react";
 const {data} = require('./test-data')
 
 export default function Statistics(){
-    return <div> This is the statistics page</div>;
+    return <div>
+        <h1>This is the statistics page</h1>
+        {
+            data.map((order) =>{
+                return <h1>{order.orderID}</h1>;
+            })
+        }
+    </div>;
 }
--- server/routes/admin.route.js
+++ server/routes/admin.route.js
@@ -4,19 +4,19 @@ const validateAdmin = require('../middlewares/validateAdmin');
 const admin = require('../controllers/admin.controller');
 
 
-router.get('/admin', validateAdmin, (req, res) => {
-    if (req.user === 'admin') {
-        console.log('WORTHY');
-    }
-    else {
-        return res.status(403).json({
-            success: 0,
-            message: "Unauthorized"
-        })
-    }
-});
+// router.get('/admin', validateAdmin, (req, res) => {
+//     if (req.user === 'admin') {
+//         console.log('WORTHY');
+//     }
+//     else {
+//         return res.status(403).json({
+//             success: 0,
+//             message: "Unauthorized"
+//         })
+//     }
+// });
 
-router.get('/admin/statistics', validateAdmin, admin.getFilteredOrders);
+router.get('/admin/statistics', admin.getFilteredOrders);
 router.get('/admin/employee', validateAdmin, admin.getAllEmployee);
 router.get('/admin/customer-info', validateAdmin, admin.displayCustomerInfo);
 
