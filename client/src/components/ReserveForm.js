2ab9d0a89b9f216119f8903a793fb2192607dd57
Lý Thanh Bách <40271986+edwardly1002@users.noreply.github.com>
2021-10-27 15:17:01 +0700

    Add initial event listeners for TextFields

--- client/src/components/ReserveForm.js
+++ client/src/components/ReserveForm.js
@@ -3,20 +3,81 @@ import { Button, TextField} from '@material-ui/core';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import {LocalizationProvider, DatePicker} from '@mui/lab';
 import '../scss/reserveform.scss';
-import React from 'react';
+import {React, useState} from 'react';
 
 function ReserveForm() {
-    const [datetime, setDatetime] = React.useState(null);
-    const [adults, setAdults] = React.useState(0);
-    const [kids, setKids] = React.useState(0);
-    const [fname, setFname] = React.useState('');
-    const [lname, setLname] = React.useState('');
-    const [phone, setPhone] = React.useState('');
-    const [email, setEmail] = React.useState('');
-    
+    const [checkForm, setCheckForm] = useState({
+        datetime: null,
+        adults: 0,
+        kids: 0
+    });
+    const [confirmForm, setConfirmForm] = useState({
+        fname: '',
+        lname: '',
+        phone: '',
+        email: ''
+    });
+
+    const handleOnchangeDatetime = (e) => {
+        setCheckForm({
+            ...checkForm,
+            datetime: e.target.value
+        });
+    }
+
+    const handleOnchangeAdults = (e) => {
+        setCheckForm({
+            ...checkForm,
+            adults: e.target.value,
+        });
+    }
+
+    const handleOnchangeKids = (e) => {
+        setCheckForm({
+            ...checkForm,
+            kids: e.target.value
+        });
+    }
+
+    const handleOnclickCheckbtn = (e) => {
+    }
+
+
+    const handleOnchangeFname = (e) => {
+        setConfirmForm({
+            ...confirmForm,
+            fname: e.target.value
+        });
+    }
+
+    const handleOnchangeLname = (e) => {
+        setConfirmForm({
+            ...confirmForm,
+            lname: e.target.value
+        });
+    }
+
+    const handleOnchangePhone = (e) => {
+        setConfirmForm({
+            ...confirmForm,
+            phone: e.target.value
+        });
+    }
+
+    const handleOnchangeEmail = (e) => {
+        setConfirmForm({
+            ...confirmForm,
+            email: e.target.value
+        });
+    }
+
+    const handleOnclickConfirmbtn = (e) => {
+    }
+
     return (
         
         <div className="reserve-form">
+            <h1>{checkForm.datetime||"hello"}</h1>
             <div  className="form-1">
                 <h1>Make a reservation</h1>
                 <div className="check-table-form">
@@ -26,6 +87,7 @@ function ReserveForm() {
                             name="datetime" 
                             type="datetime-local"
                             fullWidth
+                            onChange = {handleOnchangeDatetime}
                             InputProps={{style: {fontSize: 14}}}
                             InputLabelProps={{style: {fontSize: 14}}}
                         />
@@ -39,6 +101,7 @@ function ReserveForm() {
                             type="number"
                             placeholder="Number of adults"
                             fullWidth
+                            onChange = {handleOnchangeAdults}
                             InputProps={{style: {fontSize: 16}}}
                             InputLabelProps={{style: {fontSize: 16}}}
                         />
@@ -51,19 +114,22 @@ function ReserveForm() {
                             type="number"
                             placeholder="Number of children"
                             fullWidth
+                            onChange = {handleOnchangeKids}
                             InputProps={{style: {fontSize: 16}}}
                             InputLabelProps={{style: {fontSize: 16}}}
                         />
                     </div>
                     <div className="form-btn-row">
-                        <Button className="form-btn" variant="contained" color="secondary">
+                        <Button 
+                            className="form-btn" variant="contained" color="secondary"
+                            onclick={handleOnclickCheckbtn}
+                        >
                             Check available tables
                         </Button>
                     </div>
                 </div>
             </div>
             <div  className="form-2">
-
                 <h1>How can we contact you?</h1>
                 <div className="personal-info-form">
                     <div className="form-cell">
@@ -72,6 +138,7 @@ function ReserveForm() {
                             label="First name"
                             name="fname" 
                             placeholder="Your first name"
+                            onChange = {handleOnchangeFname}
                             InputProps={{style: {fontSize: 16}}}
                             InputLabelProps={{style: {fontSize: 16}}}
                         />
@@ -82,6 +149,7 @@ function ReserveForm() {
                             label="Last name"
                             name="lname" 
                             placeholder="Your last name"
+                            onChange = {handleOnchangeLname}
                             InputProps={{style: {fontSize: 16}}}
                             InputLabelProps={{style: {fontSize: 16}}}
                         />
@@ -93,6 +161,7 @@ function ReserveForm() {
                             name="phone" 
                             type="tel"
                             placeholder="Your phone number"
+                            onChange = {handleOnchangePhone}
                             InputProps={{style: {fontSize: 16}}}
                             InputLabelProps={{style: {fontSize: 16}}}
                         />
@@ -104,12 +173,16 @@ function ReserveForm() {
                             name="email" 
                             type="email"
                             placeholder="Your email address"
+                            onChange = {handleOnchangeEmail}
                             InputProps={{style: {fontSize: 16}}}
                             InputLabelProps={{style: {fontSize: 16}}}
                         />
                     </div>
                     <div className="form-btn-row">
-                        <Button className="form-btn" variant="contained" color="secondary">
+                        <Button 
+                            className="form-btn" variant="contained" color="secondary"
+                            onclick={handleOnclickConfirmbtn}
+                        >
                             Confirm reservation
                         </Button>
                     </div>
