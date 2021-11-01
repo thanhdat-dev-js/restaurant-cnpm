3b97cb280651be306619122ccdc925299036b24c
tvpluss <truongphuocst310301@gmail.com>
2021-11-01 08:45:49 +0700

    added getStatistic.js

---
+++ client/src/midlewares/getStatistic.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+export default function getOrder(status = null, process = 'cooking') {
+    try {
+        const token = localStorage.getItem('TOKEN') || null;
+        if (token) {
+            let reqOptions = {
+                url: `http://localhost:4000/order?status=${status}&&process=${process}`,
+                params: { token },
+                method: "GET",
+            }
+            return axios.request(reqOptions)
+        }
+        else return null;
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
