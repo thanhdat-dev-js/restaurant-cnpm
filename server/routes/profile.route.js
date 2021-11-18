16f9b056f43d605252c64b079cf4f204d248d18d
tvpluss <truongphuocst310301@gmail.com>
2021-11-18 18:24:36 +0700

    modified Profile backend

--- server/routes/profile.route.js
+++ server/routes/profile.route.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const validateToken = require('../middlewares/validateToken');
 const profile = require('../controllers/profile.controller');
 
-router.get('/profile', profile.getProfile );
-router.post('/profile', profile.postProfile );
+router.get('/profile',validateToken, profile.getProfile );
+router.post('/profile',validateToken, profile.postProfile );
 
 // router.get('/reserve', validateToken, reserve.getReserve);
 
