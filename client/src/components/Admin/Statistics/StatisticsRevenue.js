760e5f08f64534840c6238873beedf7d1bac9816
To Hoa <tohoast09@gmail.com>
2021-11-20 20:30:28 +0700

    fix wr async in statistics..

--- client/src/components/Admin/Statistics/StatisticsRevenue.js
+++ client/src/components/Admin/Statistics/StatisticsRevenue.js
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 // import { useMemo } from "react";
-import {Bar} from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 
 const dateDiff = (startDate, endDate) => {
     const date1 = startDate;
@@ -10,9 +10,9 @@ const dateDiff = (startDate, endDate) => {
     return diffDays
 }
 
-const StatisticsRevenue = (props) =>{
+const StatisticsRevenue = (props) => {
 
-    const {startDate, endDate, data} = props;
+    const { startDate, endDate, data } = props;
     console.log(data);
     const numOfDays = dateDiff(startDate, endDate) + 1;
     const [graphData, setGraphData] = useState(Array.from(Array(3), _ => Array(numOfDays).fill(0)));
@@ -20,64 +20,65 @@ const StatisticsRevenue = (props) =>{
     // arr[0] for Day
     // arr[1] for Confirmed
     // arr[2] for Cancelled
-    
-    useEffect(async () => {
-    //i is the number of day in the data
-    for(let i = 0; i < numOfDays; i++){
-        let Day = new Date(endDate - 86400000*i).setHours(0,0,0,0);
-        arr[0][numOfDays - i-1] = new Date(Day).toLocaleDateString();
-    }
-    await data.forEach((order) =>{
-        const dayOfOrder = new Date(order.updatedAt).setHours(0,0,0,0);
-        const index = arr[0].findIndex((day) => {
-            return day === new Date(dayOfOrder).toLocaleDateString(); 
-        })
-        if (order.status === "confirmed"){
-            arr[1][index]=arr[1][index] + order.total;
+
+    useEffect(() => {
+        async function fetchData() {
+            for (let i = 0; i < numOfDays; i++) {
+                let Day = new Date(endDate - 86400000 * i).setHours(0, 0, 0, 0);
+                arr[0][numOfDays - i - 1] = new Date(Day).toLocaleDateString();
+            }
+            await data.forEach((order) => {
+                const dayOfOrder = new Date(order.updatedAt).setHours(0, 0, 0, 0);
+                const index = arr[0].findIndex((day) => {
+                    return day === new Date(dayOfOrder).toLocaleDateString();
+                })
+                if (order.status === "confirmed") {
+                    arr[1][index] = arr[1][index] + order.total;
+                }
+            })
+            setGraphData(arr);
         }
-    })
-    setGraphData(arr);
-    // console.log(arr);
-    // setGData();
+        fetchData();
+
     }, [data]);
     return (
-    <div>
-        <h1>Bảng thống kê doanh thu</h1>
-        <Bar
-            data = {{
-                labels: graphData[0],
-                datasets: [
-                    {
-                        label: 'Doanh Thu',
-                        data: graphData[1],
-                        backgroundColor:[
-                            'rgba(255, 205, 71, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 182, 71, 1)',
-                        ],
-                        borderWidth: 1,
-                        
-                    },
-                    
-                ]
-            }}
-            width={300}
-            height={100}
-            options = {{
-                maintainAspectRatio: true,
-                scales: {
-                    x: {
-                        stacked: true
-                    },
-                    y: {
-                        stacked: true,
-                        beginAtZero: true
+        <div>
+            <h1>Bảng thống kê doanh thu</h1>
+            <Bar
+                data={{
+                    labels: graphData[0],
+                    datasets: [
+                        {
+                            label: 'Doanh Thu',
+                            data: graphData[1],
+                            backgroundColor: [
+                                'rgba(255, 205, 71, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255, 182, 71, 1)',
+                            ],
+                            borderWidth: 1,
+
+                        },
+
+                    ]
+                }}
+                width={300}
+                height={100}
+                options={{
+                    maintainAspectRatio: true,
+                    scales: {
+                        x: {
+                            stacked: true
+                        },
+                        y: {
+                            stacked: true,
+                            beginAtZero: true
+                        }
                     }
-                }
-            }}
-        ></Bar>
-    </div>
+                }}
+            ></Bar>
+        </div>
     )
 }
 
